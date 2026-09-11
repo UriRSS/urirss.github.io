f667a093b9bac021b3553e36afccdbfccd4f8c7b
Alikeev Urmat <96419806+UriRSS@users.noreply.github.com>
2026-09-11 15:05:10 +0600

    Deploy finalized portfolio with resources and fixed 404 page

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Localization;
 using Microsoft.JSInterop;
 using MyPortfolio;
 using System.Globalization;
